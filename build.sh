cb85928b05a4b3d4116fbb6d7ac1bfc8f161cd27
Adrian Fedoreanu <adrian.fedoreanu@gmail.com>
2016-10-14 19:46:06 +0200

    Test tcp logs

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 apk add --update go git mercurial build-base
-mkdir -p /go/src/github.com/gliderlabs
-cp -r /src /go/src/github.com/gliderlabs/logspout
-cd /go/src/github.com/gliderlabs/logspout
+mkdir -p /go/src/github.com/phedoreanu
+cp -r /src /go/src/github.com/phedoreanu/logspout
+cd /go/src/github.com/phedoreanu/logspout
 export GOPATH=/go
 go get
 go build -ldflags "-X main.Version $1" -o /bin/logspout
--- modules.go
+++ modules.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  _ "github.com/looplab/logspout-logstash"
-  _ "github.com/gliderlabs/logspout/transports/udp"
-  _ "github.com/gliderlabs/logspout/transports/tcp"
+  _ "github.com/phedoreanu/logspout-logstash-adapter"
+  _ "github.com/phedoreanu/logspout/transports/udp"
+  _ "github.com/phedoreanu/logspout/transports/tcp"
 )
